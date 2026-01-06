04d384e7d66b199b7b820a940d61fd7a8906fc12
John Zuccarelli <j.zuccarelli22@gmail.com>
2026-01-05 21:27:11 -0500

    add route for retrieving todo

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.get("/todos", (req, res) => {
   res.json({ todos });
 });
 
+app.get("/todos/:id", (req, res) => {
+  const { id } = req.params;
+  const todo = todos.find((item) => item.id === Number(id));
+
+  if (!todo) {
+    return res.status(404).send("Todo not found...");
+  }
+  res.json({ todo });
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000....");
 });
