dd50f32693861dceb46cdb7658c04fa2cdf11076
John Zuccarelli <j.zuccarelli22@gmail.com>
2026-01-19 20:04:39 -0500

    enable ssl on db connection

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const { Pool } = require("pg");
 
+const prod = process.env.NODE_ENV === "production";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: prod ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
